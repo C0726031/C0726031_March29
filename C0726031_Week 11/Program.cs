f0ddfb1a14dff19ece63cc8b071b4969de964d20
visualstudio <visualstudio@122-PC10>
2019-03-29 19:59:49 -0400

    7.55 pm

--- C0726031_Week 11/Program.cs
+++ C0726031_Week 11/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace C0726031_Week_11
@@ -9,16 +10,20 @@ namespace C0726031_Week_11
     class Program
     {
         static void Main(string[] args)
+
         {
-            HelloWorld hw = new HelloWorld();
-            hw.Hello();
+
+            Console.WriteLine("Downloading file");
+            Download();
+            Console.ReadLine();
+        }
+
+        static void Download()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Download complete");
+
         }
     }
-    class HelloWorld
-    {
-        public void Hello()
-        {
-            Console.WriteLine("HelloWorld");
-        }
-    } 
 }
+
